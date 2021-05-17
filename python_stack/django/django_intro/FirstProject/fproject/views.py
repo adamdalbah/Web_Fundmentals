d3336f53818da3aab899ce55f5bc747230a0ea96
Adam Dalbah <adamdalbah@gmail.com>
2021-05-17 14:49:57 +0300

    neww

--- python_stack/django/django_intro/FirstProject/fproject/views.py
+++ python_stack/django/django_intro/FirstProject/fproject/views.py
@@ -9,7 +9,7 @@ def index(request):
 def new(request):
     return HttpResponse("placeholder to display a new form to create a new blog")
 def create(request):
-    return redirect("/blogs/create")
+    return redirect("/")
 def show(request, num):
     return HttpResponse(f"placeholder to display blog number: {num}")
 def edit(request, num):
@@ -19,6 +19,7 @@ def destroy(request,num):
 def json(request ):
     data = {'title':'My first blog',
             'content':'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
+            
     }
     return JsonResponse(data)
 

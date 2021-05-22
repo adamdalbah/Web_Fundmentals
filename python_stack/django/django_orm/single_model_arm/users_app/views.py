c7b676cb794508510e948b1b03a28dfeec7464be
Adam Dalbah <adamdalbah@gmail.com>
2021-05-22 23:34:14 +0300

    done

--- python_stack/django/django_orm/single_model_arm/users_app/views.py
+++ python_stack/django/django_orm/single_model_arm/users_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from typing import AsyncGenerator, ContextManager
+from django.shortcuts import render, redirect, HttpResponse
+from .models import User
+def index(request):
+    context = {
+        'users' : User.objects.all() 
+    }
+    return render(request, "index.html", context)
+
+def add(request):
+    if request.method == "POST":
+        User.objects.create(first_name=request.POST['fname'], last_name=request.POST['lname'],
+        email_address=request.POST['email'], age = request.POST['age'])
+    return redirect("/")
+    
+
 
 # Create your views here.

9acbf6a3da8db34fd86e3a9439515faea50ea2c5
Adam Dalbah <adamdalbah@gmail.com>
2021-05-23 23:42:19 +0300

    Done with it

--- python_stack/django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/views.py
+++ python_stack/django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render, redirect
+from .models import *
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'dojos': Dojo.objects.all(),
+        'ninjaz': Ninja.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def add(request):
+    if request.method == 'POST':
+        if request.POST['form'] == 'dojo' :
+            Dojo.objects.create(name=request.POST['name'], city=request.POST['city'], state=request.POST['state'])
+        if request.POST['form'] == 'ninja':
+            Ninja.objects.create(first_name=request.POST['fname'], last_name=request.POST['lname'], dojo = Dojo.objects.get(name=request.POST['dojoz']) )
+
+
+
+    return redirect('/')
+
 

a1acd2f65cda6e7753ce47b9e6d242dfa8d7cd06
Adam Dalbah <adamdalbah@gmail.com>
2021-06-08 23:38:55 +0300

    first assignment

---
+++ JavaFun/FirstJava.java
@@ -0,0 +1,8 @@
+public class FirstJava {
+	public static void main(String[] args){
+		String name = "Adam Dalbah";
+		String home_town = "Tulkarem";
+		int age = 24;
+		System.out.format("My name is %s \nIam %d years old \nMy hometown is %s", name, age, home_town);
+	}
+}
